b0c147f42dfe9e9868ad17f157dc90bc1a07706c
kazrias <ka6rias@gmail.com>
2024-08-25 17:38:55 +0400

    Project start

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { Provider } from 'react-redux'
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={{}}>
+    <App />
+  </Provider>
+)
