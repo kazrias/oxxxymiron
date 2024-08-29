9fabb80fd5ba052706b336ecf353951fa32d919c
kazrias <ka6rias@gmail.com>
2024-08-29 23:11:55 +0400

    Add news section

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
-
+import React, { useCallback, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getNewsItems } from '../../reducers/newsReducer';
+import { Section } from '../Section/Section';
+import { SectionTitle } from '../SectionTitle/SectionTitle';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Icon } from '../Icon/Icon';
+import { SLIDER_BUTTON_TYPES } from '../../utils/constants';
 export const News = () => {
+  const { PREV, NEXT } = SLIDER_BUTTON_TYPES;
+  const dispatch = useDispatch();
+  const { items = [], isLoading = false } = useSelector(({ news }) => news);
+  const sliderRef = useRef(null);
+  useEffect(() => {
+    dispatch(getNewsItems());
+  }, [dispatch]);
+  const handleButtonClick = useCallback(type => {
+    if (!sliderRef.current) return;
+    const { swiper } = sliderRef.current;
+    type === NEXT ? swiper.slideNext() : swiper.slidePrev();
+  }, []);
   return (
-    <div>News</div>
-  )
-}
+    <Section className='news-section'>
+      <div className='container'>
+        <SectionTitle text='Новости' />
+        {isLoading ? (
+          'loading'
+        ) : (
+          <Swiper
+            ref={sliderRef}
+            spaceBetween={24}
+            slidesPerView={4}
+            className='news'
+            navigation
+            modules={[Navigation]}
+          >
+            {items.map(({ title, sys: { id }, cover: { url } }, i) => (
+              <SwiperSlide key={id}>
+                <ScrollAnimation animateIn='fadeInLeft' delay={i * 50}>
+                  <Link to={`/news/${id}`} className='news-item'>
+                    <div className='news-item__img'>
+                      <img src={url} alt='news cover' />
+                    </div>
+                    <h3 className='news-item__title'>{title}</h3>
+                  </Link>
+                </ScrollAnimation>
+              </SwiperSlide>
+            ))}
+            <div className='navigation'>
+              <div
+                className='navigation-button navigation-prev'
+                onClick={() => {
+                  handleButtonClick(PREV);
+                }}
+              >
+                <Icon name='slider-arrow' width={64} height={64} />
+              </div>
+              <div
+                className='navigation-button navigation-next'
+                onClick={() => {
+                  handleButtonClick(NEXT);
+                }}
+              >
+                <Icon name='slider-arrow' width={64} height={64} />
+              </div>
+            </div>
+          </Swiper>
+        )}
+      </div>
+    </Section>
+  );
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tourReducer from "../reducers/tourReducer";
 import tracksReducer from "../reducers/tracksReducer";
+import newsReducer from "../reducers/newsReducer"
 export const store = configureStore({
   reducer: {
     tour: tourReducer,
-    tracks: tracksReducer
+    tracks: tracksReducer,
+    news: newsReducer
   },
   devTools: true
 })
